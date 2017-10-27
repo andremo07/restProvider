d90045a8ee5af4274f38142a1d376ed207499cef
Andre Oliveira <andre.oliveira@imusica.com.br>
2017-10-27 15:02:07 -0200

    adicionando o método de adição de header

--- src/main/java/br/com/trendsoftware/restProvider/http/client/RestClient.java
+++ src/main/java/br/com/trendsoftware/restProvider/http/client/RestClient.java
@@ -3,13 +3,12 @@ package br.com.trendsoftware.restProvider.http.client;
 
 import com.ning.http.client.AsyncHttpClient;
 import com.ning.http.client.AsyncHttpClient.BoundRequestBuilder;
-
-import br.com.trendsoftware.restProvider.exception.RestClientException;
-
 import com.ning.http.client.AsyncHttpClientConfig;
 import com.ning.http.client.FluentCaseInsensitiveStringsMap;
 import com.ning.http.client.FluentStringsMap;
 import com.ning.http.client.Response;
+
+import br.com.trendsoftware.restProvider.exception.RestClientException;
 
 public abstract class RestClient {
 
@@ -32,51 +31,73 @@ public abstract class RestClient {
 	private BoundRequestBuilder prepareGet(String path, FluentStringsMap params) {
 
 		BoundRequestBuilder requestBuilder = http.prepareGet(getApiUrl() + path);
-		addDefaulRequestHeader(requestBuilder);
+		setDefaulRequestHeader(requestBuilder);
 		requestBuilder.setQueryParams(params);
 		return requestBuilder;
 	}
 	
 	private BoundRequestBuilder prepareGet(String path, FluentStringsMap params, FluentCaseInsensitiveStringsMap headers) {
 		BoundRequestBuilder requestBuilder = http.prepareGet(getApiUrl() + path);
-		addDefaulRequestHeader(requestBuilder);
+		setDefaulRequestHeader(requestBuilder);
 		requestBuilder.setQueryParams(params);
-		requestBuilder.setHeaders(headers);
+		addHeaders(requestBuilder,headers);
 		return requestBuilder;
 	}
 
-	private BoundRequestBuilder prepareDelete(String path,
-			FluentStringsMap params) {
+	private BoundRequestBuilder prepareDelete(String path,FluentStringsMap params) {
 
 		BoundRequestBuilder requestBuilder = http.prepareDelete(getApiUrl() + path);
-		addDefaulRequestHeader(requestBuilder);
+		setDefaulRequestHeader(requestBuilder);
 		if(params!=null)
 			requestBuilder.setQueryParams(params);
 		return requestBuilder;
+		
+	}
+	
+	private BoundRequestBuilder prepareDelete(String path,FluentStringsMap params,FluentCaseInsensitiveStringsMap headers) {
+
+		BoundRequestBuilder requestBuilder = http.prepareDelete(getApiUrl() + path);
+		setDefaulRequestHeader(requestBuilder);
+		addHeaders(requestBuilder,headers);
+		if(params!=null)
+			requestBuilder.setQueryParams(params);
+		return requestBuilder;
+		
 	}
 	
 	private BoundRequestBuilder prepareDelete(String path) {
 
 		BoundRequestBuilder requestBuilder = http.prepareDelete(getApiUrl() + path);
-		addDefaulRequestHeader(requestBuilder);
+		setDefaulRequestHeader(requestBuilder);
 		return requestBuilder;
 	}
 
-	private BoundRequestBuilder preparePost(String path,
-			FluentStringsMap params, String body) {
+	private BoundRequestBuilder preparePost(String path, FluentStringsMap params, String body) {
 		BoundRequestBuilder requestBuilder = http.preparePost(getApiUrl() + path);
-		addDefaulRequestHeader(requestBuilder);
+		setDefaulRequestHeader(requestBuilder);
+		if(body!=null && !body.isEmpty()){
+			requestBuilder.setBody(body);
+			requestBuilder.setBodyEncoding("UTF-8");
+		}
+		if(params!=null)
+			requestBuilder.setQueryParams(params);
+		return requestBuilder;
+	}
+	
+	private BoundRequestBuilder preparePost(String path, FluentStringsMap params, FluentCaseInsensitiveStringsMap headers, String body) {
+		BoundRequestBuilder requestBuilder = http.preparePost(getApiUrl() + path);
+		setDefaulRequestHeader(requestBuilder);
+		addHeaders(requestBuilder,headers);
 		if(body!=null && !body.isEmpty())
 			requestBuilder.setBody(body);
 		if(params!=null)
 			requestBuilder.setQueryParams(params);
 		return requestBuilder;
 	}
-
-	private BoundRequestBuilder preparePut(String path,
-			FluentStringsMap params, String body) {
+	
+	private BoundRequestBuilder preparePut(String path,FluentStringsMap params, String body) {
 		BoundRequestBuilder requestBuilder = http.preparePut(getApiUrl() + path);
-		addDefaulRequestHeader(requestBuilder);
+		setDefaulRequestHeader(requestBuilder);
 		requestBuilder.setBody(body);
 		if(body!=null && !body.isEmpty()){
 			requestBuilder.setBody(body);
@@ -86,12 +107,31 @@ public abstract class RestClient {
 			requestBuilder.setQueryParams(params);
 		return requestBuilder;
 	}
+	
+	private BoundRequestBuilder preparePut(String path,FluentStringsMap params,FluentCaseInsensitiveStringsMap headers,String body) {
+		BoundRequestBuilder requestBuilder = http.preparePut(getApiUrl() + path);
+		setDefaulRequestHeader(requestBuilder);
+		addHeaders(requestBuilder,headers);
+		if(body!=null && !body.isEmpty()){
+			requestBuilder.setBody(body);
+			requestBuilder.setBodyEncoding("UTF-8");
+		}
+		if(params!=null)
+			requestBuilder.setQueryParams(params);
+		return requestBuilder;
+	}
 
-	public void addDefaulRequestHeader(BoundRequestBuilder requestBuilder){
+	public void setDefaulRequestHeader(BoundRequestBuilder requestBuilder){
 		FluentCaseInsensitiveStringsMap headersMap = new FluentCaseInsensitiveStringsMap();
 		headersMap.add("Accept", "application/json");
 		headersMap.add("Content-Type", "application/json");
 		requestBuilder.setHeaders(headersMap);
+	}
+	
+	
+	public void addHeaders(BoundRequestBuilder requestBuilder, FluentCaseInsensitiveStringsMap headers){
+		if(!headers.isEmpty())
+			headers.keySet().forEach(key -> requestBuilder.addHeader(key, headers.getFirstValue(key)));	
 	}
 	
 	public Response get(String path) throws RestClientException {
@@ -140,6 +180,22 @@ public abstract class RestClient {
 
 		return response;
 	}
+	
+	
+	public Response post(String path, FluentStringsMap params,FluentCaseInsensitiveStringsMap headers, String body) throws RestClientException {
+
+		BoundRequestBuilder r = preparePost(path, params, headers, body);
+
+		Response response;
+		try {
+			response = r.execute().get();
+		} catch (Exception e) {
+			throw new RestClientException(e.getMessage());
+		}
+
+
+		return response;
+	}
 
 	public Response put(String path, FluentStringsMap params, String body) throws RestClientException {
 
@@ -154,7 +210,34 @@ public abstract class RestClient {
 
 		return response;
 	}
+	
+	public Response put(String path, FluentStringsMap params, FluentCaseInsensitiveStringsMap headers, String body) throws RestClientException {
 
+		BoundRequestBuilder r = preparePut(path, params,headers,body);
+
+		Response response;
+		try {
+			response = r.execute().get();
+		} catch (Exception e) {
+			throw new RestClientException(e.getMessage());
+		}
+
+		return response;
+	}
+
+	public Response delete(String path, FluentStringsMap params,FluentCaseInsensitiveStringsMap headers) throws RestClientException {
+		BoundRequestBuilder r = prepareDelete(path, params,headers);
+
+		Response response;
+		try {
+			response = r.execute().get();
+		} catch (Exception e) {
+			throw new RestClientException(e.getMessage());
+		}
+
+		return response;
+	}
+	
 	public Response delete(String path, FluentStringsMap params) throws RestClientException {
 		BoundRequestBuilder r = prepareDelete(path, params);
 
